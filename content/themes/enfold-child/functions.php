36d949ae2de6181aa54363e5dde763d981e964bb
forrest <forrest.clark@iolo.com>
2019-11-29 11:18:57 -0800

    remove a3-lazy-loader doesn't handld background images

--- content/themes/enfold-child/functions.php
+++ content/themes/enfold-child/functions.php
@@ -4,3 +4,18 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
+add_action('wp_head', 'preconnect_wp_head');
+function preconnect_wp_head(){
+    //Close PHP tags 
+    ?>
+	<link rel="dns-prefetch" href="https://www.google-analytics.com/">
+	<link rel="preconnect" href="https://www.google-analytics.com/">
+        <link rel="dns-prefetch" href="https://www.googletagmanager.com/">
+        <link rel="preconnect" href="https://www.googletagmanager.com/">
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com/">
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+
+
+    <?php //Open PHP tags
+}
+
